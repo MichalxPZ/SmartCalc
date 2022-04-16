7cb0afd7f9fce06fea33d020c8fd528863140d32
MichalxPZ <michauz2001@gmail.com>
2022-04-16 11:44:44 +0200

    add core-ui module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     kapt(DaggerHilt.hiltCompiler)
 
     implementation(project(Modules.core))
+    implementation(project(Modules.coreUI))
     implementation(project(Modules.calculatorPresentation))
     implementation(project(Modules.calculatorDomain))
 
--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -4,6 +4,7 @@ object Modules {
     const val app = ":app"
 
     const val core = ":core"
+    const val coreUI = ":core-ui"
 
     const val calculatorDomain = ":calculator:calculator_domain"
     const val calculatorPresentation = ":calculator:calculator_presentation"
